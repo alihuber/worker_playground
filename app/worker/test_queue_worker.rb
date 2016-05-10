dbe252e61b42d1099ca0d1c757ecb25272a3c4db
Alexander Huber <alih83@gmx.de>
2016-05-10 14:16:00 +0200

    added test queue worker exceptions for testing

--- app/worker/test_queue_worker.rb
+++ app/worker/test_queue_worker.rb
@@ -1,6 +1,11 @@
 class TestQueueWorker < Gundog::ApplicationWorker
 
   def call
-    puts "processing #{json} with object #{self.object_id}"
+    i = json.to_i
+    if i == 2
+      raise RuntimeError, "Error!"
+    else
+      puts "#{Time.zone.now.to_s}  processing #{json} with object #{self.object_id}"
+    end
   end
 end
