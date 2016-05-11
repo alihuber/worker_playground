a471c3be720f9fa6ccca7dc96a8ed0297186b722
Alexander Huber <alih83@gmx.de>
2016-05-11 15:38:50 +0200

    removed rails_helper

--- app/worker/concurrency_queue_worker.rb
+++ app/worker/concurrency_queue_worker.rb
@@ -2,7 +2,8 @@ class ConcurrencyQueueWorker < Gundog::ApplicationWorker
 
 
   def call
-    puts "#{Time.zone.now.to_s}  processing #{json} with object #{self.object_id}"
+    puts "#{Time.zone.now.to_s}  processing #{json} with "\
+      "object #{self.object_id}"
     dataset_id  = json[0]
     new_address = json[1]
     dataset = Dataset.where(id: dataset_id).first
